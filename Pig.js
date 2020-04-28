015c5c6c07851f9eb67318242a79292b93219912
Shaurya0802 <shauryaakhileshsharma@gmail.com>
2020-04-28 17:21:36 +0530

    C32

--- Pig.js
+++ Pig.js
@@ -2,10 +2,16 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
-    this.Visiblity = 255;
+    this.Visibility = 255;
   }
 
- display(){
+  score(){
+    if(this.Visibility < 0 && this.Visibility > -1005){
+      score++;
+    }
+  }
+
+  display(){
    //console.log(this.body.speed);
    if(this.body.speed < 3){
     super.display();
